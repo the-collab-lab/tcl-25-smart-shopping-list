48bda3d43bc3ddffcae8c9097b15871f5830c494
Katia <ekaterina.utochkina2@gmail.com>
2021-04-05 11:24:52 +0300

    Add jsx

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,12 @@
 import React, { useState, Fragment } from 'react';
 
-const Counter = () => {};
+const Counter = () => {
+  return (
+    <Fragment>
+      <button>Increment</button>
+      <p>0</p>
+    </Fragment>
+  );
+};
 
 export default Counter;
