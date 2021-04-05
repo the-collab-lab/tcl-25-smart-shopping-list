280df28e20e58827bdb0c25175d363ca1b8b77b9
Katia <ekaterina.utochkina2@gmail.com>
2021-04-05 11:33:15 +0300

    Retrieve and monitor collection values with useCollectionData

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,6 +7,13 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  const [countersCollection, loading, error] = useCollectionData(
+    db.collection('counters'),
+    {
+      idField: 'id',
+    },
+  );
+
   const handleClick = () => {
     setCounter((c) => c + 1);
     db.collection('counters').add({
