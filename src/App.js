6174461ac81bdc32a209a69ada9ed97fc33b3c0e
Katia <ekaterina.utochkina2@gmail.com>
2021-04-09 14:38:20 +0300

    Add return statement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
-const App = () => <Counter />;
+const App = () => {
+  return <Counter />;
+};
 
 export default App;
