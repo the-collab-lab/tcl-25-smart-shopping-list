744de5cfff60d4bd3fea9f66d81b71a7d31f6cb8
Katia <ekaterina.utochkina2@gmail.com>
2021-04-12 22:26:47 +0300

    Use token’s value stored in the browser local storage to set token' initial state. Otherwise, default initial state to empty string

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react';
 import getToken from '../lib/tokens';
 
 const Home = () => {
+  const [token, setToken] = useState(
+    JSON.parse(localStorage.getItem('token' || '')),
+  );
+
   return (
     <main>
       <button className="button">Create a Shopping List</button>
