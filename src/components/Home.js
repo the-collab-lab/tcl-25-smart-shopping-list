fc9db0f0732fafa06f18cb815551664a5779d739
Katia <ekaterina.utochkina2@gmail.com>
2021-04-12 22:19:15 +0300

    Define Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <button className="button">Create a Shopping List</button>
+    </main>
+  );
+};
+
+export default Home;
