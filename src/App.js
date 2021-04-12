f8ce372853bb4d33cbe74da0268ed9a6f7b30dc8
Katia <ekaterina.utochkina2@gmail.com>
2021-04-12 22:19:57 +0300

    Remove Counter component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddView from './components/AddView';
 import ListView from './components/ListView';
 import Navigation from './components/Navigation';
-import Counter from './components/Counter';
+
 import './App.css';
 
 const App = () => {
@@ -13,7 +13,6 @@ const App = () => {
       <Switch>
         <Route path="/add-view" component={AddView} exact />
         <Route path="/list-view" component={ListView} exact />
-        <Counter />
       </Switch>
       <Navigation />
     </BrowserRouter>
