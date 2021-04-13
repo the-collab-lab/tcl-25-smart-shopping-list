d73cbd357b4325447611dacc36ab2faf9f2ea948
Katia <ekaterina.utochkina2@gmail.com>
2021-04-13 08:04:05 +0300

    Add Counter component functionality

--- src/components/AddView.js
+++ src/components/AddView.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 
-import Counter from './Counter';
+import { db } from '../lib/firebase';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const AddView = () => {
+  const [counter, setCounter] = useState(1);
+
+  const [countersCollection] = useCollectionData(db.collection('counters'), {
+    idField: 'id',
+  });
+
+  const sortedCollection =
+    countersCollection &&
+    [...countersCollection].sort((a, b) => a.value - b.value);
+
+  const handleAdd = () => {
+    setCounter((c) => c + 1);
+    db.collection('counters').add({
+      value: counter,
+    });
+  };
+
   return (
-    <main>
-      <Counter />
-    </main>
+    <Fragment>
+      <div className="wrapper">
+        <div className="wrapper__button">
+          <button onClick={handleAdd} className="button">
+            Add
+          </button>
+        </div>
+        <ul className="wrapper__collection">
+          {sortedCollection &&
+            sortedCollection.map((item) => <li key={item.id}>{item.value}</li>)}
+        </ul>
+      </div>
+    </Fragment>
   );
 };
 
