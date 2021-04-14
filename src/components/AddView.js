fbda9eef0ec282eedf6f4cbc46c46dc9bc0ec34d
ciradu2204 <c.iradukund@alustudent.con>
2021-04-14 19:18:57 +0200

    created a form that allows a user to enter the item name and how soon they are likely to buy it again

--- src/components/AddView.js
+++ src/components/AddView.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddView = () => {
+  const [state, setState] = useState({
+    purchaseItem: '',
+    howSoon: '7',
+    lastPurchaseDate: null,
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <main>
-      <h1>Add Item</h1>
-    </main>
+    <>
+      <form className="form-container">
+        <label htmlFor="purchaseItem">Purchase Item Name:</label>
+        <input
+          type="text"
+          name="purchaseItem"
+          value={state.purchaseItem}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="howSoon">
+          How soon are you likely to buy it again?:<br></br>
+        </label>
+        <select
+          name="howSoon"
+          value={state.howSoon}
+          onBlur={handleChange}
+          onChange={handleChange}
+        >
+          <option value="7">Soon (in the next 7 days)</option>
+          <option value="14">Kind of soon (in the next 14 days)</option>
+          <option value="30">Not soon (in the next 30 days)</option>
+        </select>
+
+        <button type="submit" className="add-button">
+          Add
+        </button>
+      </form>
+    </>
   );
 };
 
