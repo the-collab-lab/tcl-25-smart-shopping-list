e027bef9de25ae6530f82d155805630480a9cb81
Chenemi Zekeri <chenemi2@gmail.com>
2021-04-15 18:32:03 +0100

    Removed conflicting code in AddView component.

--- src/components/AddView.js
+++ src/components/AddView.js
@@ -1,41 +1,10 @@
-import React, { useState, Fragment } from 'react';
-
-import { db } from '../lib/firebase';
-
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import React from 'react';
 
 const AddView = () => {
-  const [counter, setCounter] = useState(1);
-
-  const [countersCollection] = useCollectionData(db.collection('counters'), {
-    idField: 'id',
-  });
-
-  const sortedCollection =
-    countersCollection &&
-    [...countersCollection].sort((a, b) => a.value - b.value);
-
-  const handleAdd = () => {
-    setCounter((c) => c + 1);
-    db.collection('counters').add({
-      value: counter,
-    });
-  };
-
   return (
-    <Fragment>
-      <div className="wrapper">
-        <div className="wrapper__button">
-          <button onClick={handleAdd} className="button">
-            Add
-          </button>
-        </div>
-        <ul className="wrapper__collection">
-          {sortedCollection &&
-            sortedCollection.map((item) => <li key={item.id}>{item.value}</li>)}
-        </ul>
-      </div>
-    </Fragment>
+    <main>
+      <h1>Add Item</h1>
+    </main>
   );
 };
 
