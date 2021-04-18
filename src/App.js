434c193dbec972d1369f2f879e8b431f609fcf33
Katia <ekaterina.utochkina2@gmail.com>
2021-04-18 03:50:25 +0300

    Import useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import AddView from './components/AddView';
