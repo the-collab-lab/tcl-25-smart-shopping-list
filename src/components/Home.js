c707b75c99bfc64b18f6c972ac84ca98c64aae7d
Katia <ekaterina.utochkina2@gmail.com>
2021-04-18 04:06:03 +0300

    Remove redundant code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
-const Home = ({ token, handleTokenCreation }) => {
-  const history = useHistory();
-
-  useEffect(() => {
-    if (token) {
-      history.push('/list-view');
-    }
-    localStorage.setItem('token', token);
-  }, [token, history]);
-
-  return (
-    <main>
-      <button onClick={handleTokenCreation} className="button__home">
-        Create a Shopping List
-      </button>
-    </main>
-  );
-};
+const Home = ({ handleTokenCreation }) => (
+  <main>
+    <button onClick={handleTokenCreation} className="button__home">
+      Create a Shopping List
+    </button>
+  </main>
+);
 
 export default Home;
