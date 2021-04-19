2b76c087c080b4377d5a82adbc0f35e4ed439613
Chenemi Zekeri <chenemi2@gmail.com>
2021-04-19 22:31:46 +0100

    Value is updated as input changes.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = (props) => (
-  <main>
-    <button onClick={props.handleTokenCreation} className="button-home">
-      Create a Shopping List
-    </button>
+const Home = (props) => {
+  const [value, setValue] = useState('');
+  const setToken = props.setToken;
 
-    <section>
-      <form action="">
-        <label htmlFor="existingToken">Share Token</label>
-        <input
-          type="text"
-          id="existingToken"
-          placeholder="Enter three word token"
-          onChange="handleChange"
-          value=""
-        />
-        <button type="submit">Join an existing list</button>
-      </form>
-    </section>
-  </main>
-);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <main>
+      <button onClick={props.handleTokenCreation} className="button-home">
+        Create a Shopping List
+      </button>
+
+      <section>
+        <form action="">
+          <label htmlFor="existingToken">Share Token</label>
+          <input
+            type="text"
+            id="existingToken"
+            placeholder="Enter three word token"
+            value={value}
+            onChange={handleChange}
+          />
+          <button type="submit">Join an existing list</button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
 export default Home;
