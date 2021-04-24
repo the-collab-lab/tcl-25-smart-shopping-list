553d9cf437f977de1f03c142cd9680a7decd6d3b
Katia <ekaterina.utochkina2@gmail.com>
2021-04-24 12:59:27 +0300

    Update transormUserInput function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,8 @@
 const transformUserInput = (item) =>
-  item.replace(/[^a-zA-Z ]/g, '').replace(/\s\s+/g, ' ');
+  item
+    .replace(/[^a-zA-Z ]/g, '')
+    .replace(/\s\s+/g, ' ')
+    .toLowerCase()
+    .trim();
 
 export default transformUserInput;
