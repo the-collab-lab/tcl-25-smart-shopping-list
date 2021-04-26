b78d28f071e2de421368e26e41db9c95995b5394
ciradu2204 <c.iradukund@alustudent.con>
2021-04-26 21:13:40 +0200

    return a prompt when list is empty

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,28 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 import ListItem from '../components/ListItem';
 
 const ListView = ({ shoppingList, loading, error }) => {
-  return (
-    <>
-      {error && <strong>Error: {JSON.stringify(error)}</strong>}
-      {loading && (
-        <div className="bouncing-loader">
-          <div></div>
-          <div></div>
-          <div></div>
+    const [shoppingListEmpty, setShoppingListEmpty] = useState(true);
+
+    if (shoppingList.items.length >= 1) {
+        setShoppingListEmpty(false);
+    }
+ 
+
+
+    return shoppingListEmpty ? (
+
+        <div>
+            <h1>No Item Added</h1>
+            <Link to= "/add-view" />
         </div>
-      )}
-      <ul className="list">
-        {shoppingList &&
-          shoppingList.map((list) =>
-            list.items.map((item) => <ListItem key={uuidv4()} item={item} />),
-          )}
-      </ul>
-    </>
-  );
+
+    ): (
+        <>
+            {error && <strong>Error: {JSON.stringify(error)}</strong>}
+            {loading && (
+                <div className="bouncing-loader">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            )}
+            <ul className="list">
+                {shoppingList &&
+                    shoppingList.map((list) =>
+                        list.items.map((item) => <ListItem key={uuidv4()} item={item} />),
+                    )}
+            </ul>
+        </>
+    );
 };
 
 export default ListView;
