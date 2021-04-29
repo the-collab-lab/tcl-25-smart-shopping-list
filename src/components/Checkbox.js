3787d83bf8e739e37a6d816aed2043da09b64926
Katia <ekaterina.utochkina2@gmail.com>
2021-04-29 23:44:33 +0300

    Capture item and shoppinglist props

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { db } from '../lib/firebase';
 
-const Checkbox = () => {
+const Checkbox = ({ item, shoppingList }) => {
   return <div></div>;
 };
 
