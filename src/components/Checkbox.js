ab9c2643a7756efc55a08fcbb282fa09748e70d9
Katia <ekaterina.utochkina2@gmail.com>
2021-04-29 23:45:15 +0300

    Create element of state to track state of checkboxes

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { db } from '../lib/firebase';
 
 const Checkbox = ({ item, shoppingList }) => {
+  const [checked, setChecked] = useState(false);
   return <div></div>;
 };
 
