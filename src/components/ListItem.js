e8cd2f72f96b8ef6642f3c101118b8179eeb997f
Katia <ekaterina.utochkina2@gmail.com>
2021-05-11 13:07:56 +0300

    Imort db and firebase

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,6 +4,9 @@ import Checkbox from '../components/Checkbox';
 import BinIcon from '../components/BinIcon';
 import Dialog from '../components/Dialog';
 
+import { db } from '../lib/firebase';
+import firebase from 'firebase';
+
 const ListItem = ({ item, shoppingList }) => (
   <li className="list__item">
     <Checkbox item={item} shoppingList={shoppingList} />
