b17cf8d967b6748a333e40ed98e0f0d62460fdce
Katia <ekaterina.utochkina2@gmail.com>
2021-05-11 13:18:09 +0300

    Update jsx

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -5,8 +5,12 @@ const Dialog = ({ onCancel, onDelete }) => (
     <div className="dialog__inner">
       <p id="dialog-content">Are you sure you want to delete this item?</p>
       <div className="btn-container">
-        <button className="btn">Yes</button>
-        <button className="btn">Cancel</button>
+        <button onClick={onDelete} className="btn">
+          Yes
+        </button>
+        <button onClick={onCancel} className="btn">
+          Cancel
+        </button>
       </div>
     </div>
   </div>
