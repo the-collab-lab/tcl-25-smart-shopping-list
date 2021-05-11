87329dd15ef6f60c846d109dc2bc7deb573a5e5d
Katia <ekaterina.utochkina2@gmail.com>
2021-05-11 13:05:43 +0300

    Add component

---
+++ src/components/Dialog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dialog = () => (
+  <div id="dialog">
+    <div className="dialog__inner">
+      <p id="dialog-content">Are you sure you want to delete this item?</p>
+      <div className="btn-container">
+        <button className="btn">Yes</button>
+        <button className="btn">Cancel</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default Dialog;
