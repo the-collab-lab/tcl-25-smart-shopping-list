c2f2df636787f965d0bff6618d2b51979a429b7d
Katia <ekaterina.utochkina2@gmail.com>
2021-05-11 13:07:33 +0300

    Import BinIcon and Dialog

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+
 import Checkbox from '../components/Checkbox';
+import BinIcon from '../components/BinIcon';
+import Dialog from '../components/Dialog';
 
 const ListItem = ({ item, shoppingList }) => (
   <li className="list__item">
