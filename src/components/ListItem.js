f2cdc2e2a10220d9f427ff8ace025d63448f5316
Chenemi Zekeri <55132738+C-Zekeri@users.noreply.github.com>
2021-05-14 14:39:42 +0100

    Removed empty braces

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,6 @@ import Checkbox from '../components/Checkbox';
 
 const ListItem = ({ index, item, shoppingList }) => (
   <li className={index} aria-label={index}>
-    {' '}
     <Checkbox item={item} shoppingList={shoppingList} />
   </li>
 );
