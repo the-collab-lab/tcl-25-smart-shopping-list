b7250265868120aba227a8474b9ad157e730456b
Katia <ekaterina.utochkina2@gmail.com>
2021-05-22 09:50:04 +0300

    Update jsx

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
   <div className="nav-container">
-    <nav>
-      <NavLink activeClassName="active" to="/add-view">
-        Add Item
-      </NavLink>
-      <NavLink activeClassName="active" to="/list-view">
-        View List
-      </NavLink>
-    </nav>
+    <div className="nav-container__inner">
+      <div className="flow-space-700">
+        <nav>
+          <ul className="nav">
+            <NavLink
+              activeClassName="active"
+              to="/add-view"
+              className="nav__link"
+            >
+              Add Item
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              to="/list-view"
+              className="nav__link"
+            >
+              View List
+            </NavLink>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
 );
 
