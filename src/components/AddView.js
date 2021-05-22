e18b2a0d0cfb83f71663866ccaab77363b624511
Katia <ekaterina.utochkina2@gmail.com>
2021-05-22 09:40:40 +0300

    Add Nav, update jsx

--- src/components/AddView.js
+++ src/components/AddView.js
@@ -87,56 +87,64 @@ const AddView = ({ shoppingList, token, collectionId }) => {
   };
 
   return (
-    <form className="form" onSubmit={handleAdd}>
-      <div className="form-row">
-        <div className="form-group">
-          <label htmlFor="name">Purchase Item Name</label>
-          <input
-            className="form-field"
-            id="name"
-            type="text"
-            name="name"
-            value={item.name}
-            onChange={handleChange}
-            placeholder={!isNameFieldEmpty ? 'Add an item' : ''}
-          />
-        </div>
+    <div className="wrapper">
+      <div className="dot-shadow panel">
+        <main className="list-view flow">
+          <form className="form" onSubmit={handleAdd}>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="name">Purchase Item Name</label>
+                <input
+                  className="form-field"
+                  id="name"
+                  type="text"
+                  name="name"
+                  value={item.name}
+                  onChange={handleChange}
+                  placeholder={!isNameFieldEmpty ? 'Add an item' : ''}
+                />
+              </div>
+            </div>
+            {hasDuplicates ? (
+              <div className="field-error">Item is already on the list</div>
+            ) : null}
+            {isNameFieldEmpty ? (
+              <div className="field-error">This field is required</div>
+            ) : null}
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="howSoon">
+                  How soon are you likely to buy it again?
+                </label>
+                <select
+                  className="form-field"
+                  id="howSoon"
+                  name="daysLeftForNextPurchase"
+                  value={item.daysLeftForNextPurchase}
+                  onBlur={handleChange}
+                  onChange={handleChange}
+                >
+                  <option value=""></option>
+                  <option value="7">Soon (in the next 7 days)</option>
+                  <option value="14">Kind of soon (in the next 14 days)</option>
+                  <option value="30">Not soon (in the next 30 days)</option>
+                </select>
+              </div>
+            </div>
+            {isFrequencyFieldEmpty ? (
+              <div className="field-error">This field is required</div>
+            ) : null}
+            <div className="form-row">
+              <button type="submit" className="add-button">
+                Add
+              </button>
+            </div>
+          </form>
+
+          <Navigation />
+        </main>
       </div>
-      {hasDuplicates ? (
-        <div className="field-error">Item is already on the list</div>
-      ) : null}
-      {isNameFieldEmpty ? (
-        <div className="field-error">This field is required</div>
-      ) : null}
-      <div className="form-row">
-        <div className="form-group">
-          <label htmlFor="howSoon">
-            How soon are you likely to buy it again?
-          </label>
-          <select
-            className="form-field"
-            id="howSoon"
-            name="daysLeftForNextPurchase"
-            value={item.daysLeftForNextPurchase}
-            onBlur={handleChange}
-            onChange={handleChange}
-          >
-            <option value=""></option>
-            <option value="7">Soon (in the next 7 days)</option>
-            <option value="14">Kind of soon (in the next 14 days)</option>
-            <option value="30">Not soon (in the next 30 days)</option>
-          </select>
-        </div>
-      </div>
-      {isFrequencyFieldEmpty ? (
-        <div className="field-error">This field is required</div>
-      ) : null}
-      <div className="form-row">
-        <button type="submit" className="add-button">
-          Add
-        </button>
-      </div>
-    </form>
+    </div>
   );
 };
 
