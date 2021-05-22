012772db43c816ccfabc10ede815686d004ae521
Katia <ekaterina.utochkina2@gmail.com>
2021-05-22 09:33:16 +0300

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import LogoIcon from './LogoIcon';
+
+const Header = () => (
+  <header class="app__header">
+    <LogoIcon />
+    <h1>Smart Shopping App</h1>
+  </header>
+);
+
+export default Header;
